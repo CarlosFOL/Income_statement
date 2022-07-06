5b41fa616372ba7ef7000cbdbd678f749ef92fad
Carlos Francisco Orejuela Lluncor <carlos_francisco_@hotmail.com>
2022-07-06 07:32:41 -0500

    save_data: Export data to a file.txt

--- income_statement.py
+++ income_statement.py
@@ -1,5 +1,12 @@
 from company import Company
 
+
+def save_data(c1_data, scenario):
+    with open(f"./{scenario}_scenary.txt","w", encoding="utf-8") as f:
+        for element in c1_data:
+            f.write(str(element))
+            f.write("\n")
+
 def others_calculations(c1, gross_profit, admin_cost):
     operative_profit = round(gross_profit - admin_cost, 2)
     net_profit = round(operative_profit - (operative_profit*c1.taxes), 2)
@@ -13,10 +20,14 @@ def income_statement(c1, scenario):
         gross_profit = round((c1.unit_income -c1.unit_cost)*c1.sales, 2)
         admin_cost = c1.payroll + c1.publicity + c1.rent + c1.services
         profits_and_extras = others_calculations(c1, gross_profit, admin_cost)
+        save_data(profits_and_extras, "actual")
+        return gross_profit, profits_and_extras
     elif scenario == "N":
         gross_profit = round((c1.unit_income -c1.unit_cost)*(c1.sales*1.15), 2)
         admin_cost = c1.payroll + (c1.publicity*2) + c1.rent + c1.services
         profits_and_extras = others_calculations(c1, gross_profit, admin_cost)
+        save_data(profits_and_extras, "actual")
+        return gross_profit, profits_and_extras
 
 if __name__=='__main__':
     company_1 = Company("Oreo", 3500, 2.5, 0.2, 1600, 800, 100, 1000, 0.18)
