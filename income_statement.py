d35ca7f98bf576a6ec48a5305afc18d59e8ad15b
Carlos Francisco Orejuela Lluncor <carlos_francisco_@hotmail.com>
2022-07-06 07:48:10 -0500

    comparation and check_scenario function: done

--- income_statement.py
+++ income_statement.py
@@ -1,5 +1,35 @@
 from company import Company
 
+def check_scenarios(a_sc, n_sc):
+    data_compare = {
+        "Net profit": 1,
+        "Margin contribution": 2,
+        "Break even point": 3,
+        "Dollar/ gain": 4
+    }
+    score_a_sc = 0
+    score_n_sc= 0
+    for k, v in data_compare.items():
+            if a_sc[v] > n_sc[v]:
+                print(f"* More {k}: ACTUAL SCENARIO")
+                score_a_sc += 1
+            elif a_sc[v] < n_sc[v]:
+                print(f"* More {k}: NEW SCENARIO")
+                score_n_sc += 1
+    return score_a_sc, score_n_sc
+
+def comparation():
+    actual_scenario = []
+    with open("./actual_scenary.txt", "r", encoding="utf-8") as f:
+           for i in f:
+            actual_scenario.append(float(i.strip('\n')))
+    new_scenario = []
+    with open("./new_scenary.txt", "r", encoding="utf-8") as f:
+            for i in f:
+                new_scenario.append(float(i.strip('\n')))
+    check_scenarios(actual_scenario, new_scenario)
+
+#Report
 def structure(company_data):
     print(f"""
     -------------------------------
@@ -18,16 +48,17 @@ def report(func):
     def wrapper(company, scenario):
         company_data = func(company, scenario)
         if scenario == "A":
-            print(f"""[Actual scenary] By calculating income statement of "{company.name}" business...""")
+            print(f"""[Actual scenario] By calculating income statement of "{company.name}" business...""")
             structure(company_data)
         elif scenario == "N":
             print(f"""
-[New scenary] By calculating income statement of "{company.name}" business...""")
+[New scenario] By calculating income statement of "{company.name}" business...""")
             structure(company_data)
     return wrapper
 
-def save_data(c1_data, scenario):
-    with open(f"./{scenario}_scenary.txt","w", encoding="utf-8") as f:
+@report
+def save_data(c1_data, type):
+    with open(f"./{type}_scenario.txt","w", encoding="utf-8") as f:
         for element in c1_data:
             f.write(str(element))
             f.write("\n")
@@ -56,5 +87,5 @@ def income_statement(c1, scenario):
 
 if __name__=='__main__':
     company_1 = Company("Oreo", 3500, 2.5, 0.2, 1600, 800, 100, 1000, 0.18)
-    income_statement(company_1, "A") #A = Actual scenary
-    income_statement(company_1, "N") #N = New scenary
+    income_statement(company_1, "A") #A = Actual scenario
+    income_statement(company_1, "N") #N = New scenario
